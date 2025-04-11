b3dc3336a021dab7e56f1e2364205fcf9034ab66
LucasCid <lucascidcanete@gmail.com>
2025-04-10 22:47:44 -0400

    validando formulario

--- app/app.js
+++ app/app.js
@@ -1,14 +1,45 @@
-function openModal() {
-    document.getElementById('modal').style.display = 'flex';
-  }
+function abrirModal() {
+  document.getElementById('modal').style.display = 'flex';
+}
+function cerrarModal() {
+  document.getElementById('modal').style.display = 'none';
+}
 
-  function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-  }
 
-  function switchTab(tab) {
-    document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
-    document.querySelectorAll('.form-section').forEach(section => section.classList.remove('active'));
-    document.querySelector(`.tab[onclick*='${tab}']`).classList.add('active');
-    document.getElementById(tab).classList.add('active');
-  }
+function cambiarPestaña(pestaña) {
+  document.querySelectorAll('.pestaña').forEach(btn => btn.classList.remove('activa'));
+  document.querySelectorAll('.seccion-formulario').forEach(sección => sección.classList.remove('activa'));
+  document.querySelector(`.pestaña[onclick*='${pestaña}']`).classList.add('activa');
+  document.getElementById(pestaña).classList.add('activa');
+}
+function mostrarPopup() {
+  const popup = document.getElementById('popup');
+  popup.classList.add('mostrar');
+
+  setTimeout(() => {
+    popup.classList.remove('mostrar');
+  }, 3000); 
+}
+
+
+function manejarEnvio() {
+ const pestañaActiva = document.querySelector('.pestaña.activa').textContent.includes('Empresa') ? 'empresa' : 'personal';
+ const formulario = document.querySelector(`#form-${pestañaActiva}`);
+
+ if (formulario.checkValidity()) {
+   cerrarModal();       
+   mostrarPopup();        
+   formulario.reset();  
+        
+ } else {
+   formulario.reportValidity();  
+}
+
+}
+
+
+document.querySelectorAll('input[name="rutEmpresa"], input[name="rutPersonal"]').forEach(input => {
+  input.addEventListener('input', () => {
+    input.value = input.value.replace(/[^0-9kK-]/g, ''); // Permite solo números, k/K y guión
+  });
+});
